4152e5c079a213ccb8750c6eb8980940edeb549e
Altered Art <cherokeedreemer@gmail.com>
2020-05-13 22:07:26 -0600

    work on the javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('container');
+const text = document.getElementById("text");
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+console.log(breatheTime, holdTime);
