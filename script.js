39348c149cb95f8e8726e17c275da0899a24b90b
Altered Art <cherokeedreemer@gmail.com>
2020-05-13 22:23:07 -0600

    finished

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
 console.log(breatheTime, holdTime);
+
+breathe();
+
+function breathe() {
+  text.innerText = "!BREATHE IN!";
+  container.className = "container grow";
+
+  setTimeout(() => {
+    text.innerText = 'HOLD';
+
+    setTimeout(() => {
+      text.innerText = '*breathe out*';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breathe, totalTime);
