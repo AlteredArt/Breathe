c0b9e6512b78ccf07be5bc6fe1f4a2bda57bc018
Jared Matta <cherokeedreemer@gmail.com>
2022-04-01 00:59:05 -0500

    begin work

---
+++ New folder/index.php
@@ -0,0 +1,24 @@
+<?php
+include("includes/config.php");
+
+//session_destroy(); LOGOUT
+
+if(isset($_SESSION['userLoggedIn'])) {
+	$userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else {
+	header("Location: register.php");
+}
+
+?>
+
+<html>
+<head>
+	<title>Welcome to BLANK!</title>
+</head>
+
+<body>
+	BLANK!
+</body>
+
+</html>
