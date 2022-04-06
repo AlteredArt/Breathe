852c7c4a1099692e5e3acad001033b0d1e61483f
Jared Matta <cherokeedreemer@gmail.com>
2022-04-06 00:46:02 -0500

    finish up login page

--- Edit/includes/config.php
+++ Edit/includes/config.php
@@ -2,9 +2,9 @@
 	ob_start();
 	session_start();
 
-	$timezone = date_default_timezone_set("Europe/London");
+	$timezone = date_default_timezone_set("America/Denver");
 
-	$con = mysqli_connect("localhost", "root", "", "spoofify");
+	$con = mysqli_connect("localhost", "root", "", "breathe");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
--- Edit/includes/navBarContainer.php
+++ Edit/includes/navBarContainer.php
@@ -2,33 +2,47 @@
 	<nav class="navBar">
 
 		<span role="link" tabindex="0" onclick="openPage('index.php')" class="logo">
-			<img src="assets/images/icons/logo.png">
+			<img src="assets/images/icons/circle.png">
 		</span>
 
 
 		<div class="group">
 
+		
 			<div class="navItem">
-				<span role='link' tabindex='0' onclick='openPage("search.php")' class="navItemLink">
-					Search
-					<img src="assets/images/icons/search.png" class="icon" alt="Search">
-				</span>
+				<span role="link" tabindex="0" onclick="openPage('settings.php')" class="navItemLink"><?php echo $userLoggedIn->getFirstAndLastName(); ?></span>
 			</div>
 
 		</div>
 
 		<div class="group">
 			<div class="navItem">
-				<span role="link" tabindex="0" onclick="openPage('browse.php')" class="navItemLink">Browse</span>
+				<span role='link' tabindex='0' onclick='openPage("index.php")' class="navItemLink">
+					Backgrounds
+					<!-- <img src="assets/images/icons/search.png" class="icon" alt="Search"> -->
+				</span>
+			</div>
+			<div class="navItem">
+				<span role='link' tabindex='0' onclick='openPage("index.php")' class="navItemLink">
+					Music
+					<!-- <img src="assets/images/icons/search.png" class="icon" alt="Search"> -->
+				</span>
+			</div>
+			<div class="navItem">
+				<span role="link" tabindex="0" onclick="openPage('browse.php')" class="navItemLink">Relax</span>
+			</div>
+			<div class="navItem">
+				<span role="link" tabindex="0" onclick="openPage('browse.php')" class="navItemLink">Sleep</span>
+			</div>
+			<div class="navItem">
+				<span role="link" tabindex="0" onclick="openPage('browse.php')" class="navItemLink">Focus</span>
 			</div>
 
 			<div class="navItem">
-				<span role="link" tabindex="0" onclick="openPage('yourMusic.php')" class="navItemLink">Your Music</span>
+				<span role="link" tabindex="0" onclick="openPage('yourMusic.php')" class="navItemLink">Your Playlists</span>
 			</div>
 
-			<div class="navItem">
-				<span role="link" tabindex="0" onclick="openPage('settings.php')" class="navItemLink"><?php echo $userLoggedIn->getFirstAndLastName(); ?></span>
-			</div>
+		
 		</div>
 
 	</nav>
