f790c0a39afd0231dbfe8a73201dfb6d7ee50c3d
Jared Matta <cherokeedreemer@gmail.com>
2022-05-05 00:08:48 -0600

    starting with relax

---
+++ Edit/relax.php
@@ -0,0 +1,26 @@
+<!------------------------------------------------------------------>
+<!-- RELAX -->
+<!------------------------------------------------------------------>
+<?php include("includes/includedFiles.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<link rel="stylesheet" href="./assets/css/index.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Relax Page</title>
+	</head>
+	<body>
+		<div id="start">
+			<div class="page-header">
+				<h3 class="page-title">Relax, Breathe!</h3>
+				<hr/>
+			</div>
+			<div class='breathe-container'>
+                hello
+			</div>
+		</div>
+	</body>
+</html>
